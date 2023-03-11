cca1d2c8a364cf8a44b96e3283b46dad03a7ed35
kart <mkartveli9@gmail.com>
2023-03-11 00:21:21 +0000

    queue

--- src/main/java/com/epam/collections/queue/ArrayDequeCreator.java
+++ src/main/java/com/epam/collections/queue/ArrayDequeCreator.java
@@ -6,5 +6,25 @@ import java.util.Queue;
 
 public class ArrayDequeCreator extends PriorityQueue<String> {
     public ArrayDeque<Integer> createArrayDeque(Queue<Integer> firstQueue, Queue<Integer> secondQueue) {
+        System.out.println(firstQueue);
+        System.out.println(secondQueue);
+        ArrayDeque<Integer> card = new ArrayDeque<>();
+        card.add(firstQueue.remove());
+        card.add(firstQueue.remove());
+        card.add(secondQueue.remove());
+        card.add(secondQueue.remove());
+
+
+        while (!firstQueue.isEmpty() | !secondQueue.isEmpty()) {
+            firstQueue.add(card.remove());
+            secondQueue.add(card.remove());
+            card.add(firstQueue.remove());
+            card.add(firstQueue.remove());
+            card.add(secondQueue.remove());
+            card.add(secondQueue.remove());
+
+        }
+        System.out.println(card);
+        return card;
     }
 }
--- src/main/java/com/epam/collections/queue/DishOrderDeterminer.java
+++ src/main/java/com/epam/collections/queue/DishOrderDeterminer.java
@@ -1,8 +1,32 @@
 package com.epam.collections.queue;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class DishOrderDeterminer {
     public List<Integer> determineDishOrder(int numberOfDishes, int everyDishNumberToEat) {
+        List<Integer> di = new ArrayList<>();
+        List<Integer> ra = new ArrayList<>();
+
+        for(int a = 1; a <= numberOfDishes; a++) {
+            ra.add(a);
+        }
+        List<Integer> rac = new ArrayList<>(ra);
+        int j = 1;
+        while (di.size() != numberOfDishes) {
+            for (int i = 0; i < ra.size(); i++) {
+                if (j % everyDishNumberToEat == 0 ) {
+                    di.add(ra.get(i));
+                    rac.remove(ra.get(i));
+                    j = 1;
+                } else {
+                    j += 1;
+
+                }
+
+            }
+            ra = new ArrayList<>(rac);
+        }
+        return di;
     }
 }
